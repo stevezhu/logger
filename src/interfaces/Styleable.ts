5e8e7bf9663c797b4b59cf7ae2c751099994b21c
Steve Zhu <st.zhu1@gmail.com>
2019-08-29 21:10:23 -0400

    Add factory function for `Styleable`

--- src/interfaces/Styleable.ts
+++ src/interfaces/Styleable.ts
@@ -3,3 +3,7 @@ import { Loggable } from './Loggable'
 export interface Styleable<T> extends Loggable<T> {
   style: string
 }
+
+export function createStyleable<T>(obj: T, style: string = ''): Styleable<T> {
+  return { toLoggableObject: () => obj, style }
+}
