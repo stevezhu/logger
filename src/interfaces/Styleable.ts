41b61c3bed12a2069d9a3af160143b7585e41187
Steve Zhu <st.zhu1@gmail.com>
2019-09-04 23:21:06 -0400

    Add documentation to `Styleable`

--- src/interfaces/Styleable.ts
+++ src/interfaces/Styleable.ts
@@ -1,9 +1,17 @@
 import { Loggable } from './Loggable'
 
+/**
+ * An interface for `Loggable` objects that can be styled when logged.
+ */
 export interface Styleable<T> extends Loggable<T> {
   style: string
 }
 
+/**
+ * Create a `Styleable` object where the loggable object is static.
+ * @param obj The object to return in `toLoggableObject`.
+ * @param style The style of the object when logged.
+ */
 export function createStyleable<T>(obj: T, style: string = ''): Styleable<T> {
   return { toLoggableObject: () => obj, style }
 }
