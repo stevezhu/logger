17d1f324aad59e61b1e8eb3fc3a4656b044a6a3c
Steve Zhu <st.zhu1@gmail.com>
2019-09-06 22:54:18 -0400

    Implement `Logger`

---
+++ src/Logger.ts
@@ -0,0 +1,68 @@
+import { logstyleRecursive as logstyle } from './logstyle'
+import { Styleable, createStyleable } from './interfaces/Styleable'
+import { Timestamp, TimestampFormat } from './Timestamp'
+
+export enum LogLevel {
+  Error = 'error',
+  Warn = 'warn',
+  Info = 'info',
+  Debug = 'debug',
+}
+
+const DEFAULT_STYLES = {
+  name: 'font-weight: bold',
+  timestamp: 'color: gray; font-style: italic;',
+
+  levels: 'color: white; text-transform: uppercase;',
+  error: 'background-color: red;',
+  warn: 'background-color: #ffe000;', // yellow
+  info: 'background-color: #66bb6a;', // green
+  debug: 'background-color: gray;',
+}
+
+export class Logger {
+  private styles: typeof DEFAULT_STYLES
+  console: Console
+  name?: Styleable<string>
+  timestamp: Timestamp
+
+  readonly error: LogFunction
+  readonly warn: LogFunction
+  readonly info: LogFunction
+  readonly debug: LogFunction
+
+  constructor({
+    console = globalThis.console,
+    timestampFormat = TimestampFormat.DateTime,
+    styles = DEFAULT_STYLES,
+  }: {
+    console?: Console
+    name?: string
+    timestampFormat?: TimestampFormat
+    styles?: Partial<typeof DEFAULT_STYLES>
+  } = {}) {
+    this.styles = Object.assign({}, DEFAULT_STYLES, styles)
+
+    this.console = console
+
+    if (name != null) {
+      this.name = createStyleable<string>(name, styles.name)
+    }
+    this.timestamp = new Timestamp(timestampFormat, styles.timestamp)
+
+    this.error = this.createLogFunction(LogLevel.Error)
+    this.warn = this.createLogFunction(LogLevel.Warn)
+    this.info = this.createLogFunction(LogLevel.Info)
+    this.debug = this.createLogFunction(LogLevel.Debug)
+  }
+
+  private createLogFunction(levelObj: LogLevel): LogFunction {
+    const level = createStyleable<LogLevel>(levelObj, this.styles[levelObj])
+
+    let substitutions = logstyle`${this.timestamp} ${level}`
+    if (this.name != null) {
+      substitutions = substitutions` [${this.name}]`
+    }
+    return this.console[levelObj].bind(this.console, ...substitutions())
+  }
+}
