e20b1cb64bbcdeb9b21800c36e1ab08e7c2189a6
Steve Zhu <st.zhu1@gmail.com>
2019-09-06 22:47:04 -0400

    Add different tsconfig files

--- jest.config.js
+++ jest.config.js
@@ -2,4 +2,7 @@ module.exports = {
   roots: ['<rootDir>/src/'],
   transform: { '^.+\\.tsx?$': 'ts-jest' },
   setupFilesAfterEnv: ['<rootDir>/jest/expect-toLog.ts'],
+  globals: {
+    'ts-jest': { tsConfig: 'tsconfig.test.json' },
+  },
 }
